a1ddefec71ba04aa597198b3a7efb09fb825a320
Sofia Ling <sofialing@live.se>
2019-12-18 15:41:50 +0100

    Updated functionality to render tracks

--- assets/script/app.js
+++ assets/script/app.js
@@ -11,22 +11,31 @@ const filterTracks = (searchResult, search) => {
 	return searchResult.filter(track => track.title.toLowerCase().includes(search));
 };
 
-const renderTracks = tracks => {
+const renderTracks = (tracks, search) => {
 	const ulListEl = document.createElement('ul');
 	ulListEl.className = 'list-group';
+	searchResultEL.innerHTML = `<h1 class="h5 text-center">Show results for <strong>"${search}"</strong></h1>`;
+
+	if (!tracks.length) {
+		searchResultEL.innerHTML += 'Nothing to display.';
+		return;
+	}
 
 	tracks.forEach(track => {
+		let duration = moment.unix(track.duration).format('m:ss');
+		console.log(duration);
 		ulListEl.innerHTML += `
 			<li class="list-group-item list-group-item-dark">
 				<img src="${track.album.cover}"
 					class="mr-3 cover-img" alt="">
 				<div>
-					<p class="mb-0">${track.title} - ${track.artist.name}</p>
+					<p class="mb-0 font-weight-bold"><a href="#">${track.title}</a> <span class="pl-1 text-muted font-weight-light">${duration}</span></p>
+					<p class="mb-0">by <a href="#">${track.artist.name}</a></p>
 					<p class="text-muted mb-0">${track.album.title}</p>
 				</div>
 			</li>`;
 	});
-	searchResultEL.innerHTML = '';
+
 	searchResultEL.append(ulListEl);
 };
 
@@ -54,7 +63,7 @@ const getSearchResults = async search => {
 const updateUI = (searchResults, search) => {
 	if (searchFilter.includes('track')) {
 		const tracks = filterTracks(searchResults.data, search);
-		renderTracks(tracks);
+		renderTracks(tracks, search);
 	}
 	if (searchFilter.includes('album')) {
 		console.log('show albums');
