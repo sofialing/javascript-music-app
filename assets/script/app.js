17c8d5ab708bbe7c8f9c7b00f9a2c36be8d1d2e8
Sofia Ling <sofialing@live.se>
2019-12-19 12:43:13 +0100

    Improved search function

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,54 +4,87 @@
  */
 const searchForm = document.querySelector('#search-form');
 const searchResultEL = document.querySelector('#search-result');
-const checkBoxes = searchForm.querySelectorAll('.form-check-input');
-let searchFilter = [];
+const albumsEl = document.querySelector('#albums');
+const artistsEl = document.querySelector('#artists');
+const tracksEl = document.querySelector('#tracks');
 
-const filterTracks = (searchResult, search) => {
-	return searchResult.filter(track => track.title.toLowerCase().includes(search));
-};
+const renderArtists = (artists, search) => {
+	artistsEl.classList.remove('d-none');
+	artistsEl.querySelector('ul').innerHTML = '';
+	artistsEl.querySelector('header a').setAttribute('data-search', search);
+	artistsEl.querySelector('header a').setAttribute('data-type', 'artist');
 
-const renderTracks = (tracks, search) => {
-	const ulListEl = document.createElement('ul');
-	ulListEl.className = 'list-group';
-	searchResultEL.innerHTML = `<h1 class="h5 text-center">Show results for <strong>"${search}"</strong></h1>`;
-
-	if (!tracks.length) {
+	if (!artists.length) {
 		searchResultEL.innerHTML += 'Nothing to display.';
 		return;
 	}
 
+	artists.forEach(artist => {
+		artistsEl.querySelector('ul').innerHTML += `
+			<li class="list-group-item list-group-item-dark">
+				<img src="${artist.picture_medium}"
+					class="mr-3 cover-img" alt="">
+				<div>
+					<p class="mb-0"><a href="#" data-artist="${artist.id}">${artist.name}</a></p>
+					<p class="text-muted mb-0">Artist</a></p>
+				</div>
+			</li>`;
+	});
+};
+
+const renderAlbums = (albums, search) => {
+	albumsEl.classList.remove('d-none');
+	albumsEl.querySelector('ul').innerHTML = '';
+	albumsEl.querySelector('header a').setAttribute('data-search', search);
+	albumsEl.querySelector('header a').setAttribute('data-type', 'album');
+
+	if (!albums.length) {
+		albumsEl.innerHTML += 'Nothing to display.';
+		return;
+	}
+
+	albums.forEach(album => {
+		albumsEl.querySelector('ul').innerHTML += `
+			<li class="list-group-item list-group-item-dark">
+				<img src="${album.cover_medium}"
+					class="mr-3 cover-img" alt="">
+				<div>
+					<p class="mb-0"><a href="#" data-album="${album.id}">${album.title}</a></p>
+					<p class="text-muted mb-0"><a href="#" data-artist="${album.artist.id}">${album.artist.name}</a></p>
+				</div>
+			</li>`;
+	});
+};
+
+const renderTracks = (tracks, search) => {
+	tracksEl.classList.remove('d-none');
+	tracksEl.querySelector('ul').innerHTML = '';
+	tracksEl.querySelector('header a').setAttribute('data-search', search);
+	tracksEl.querySelector('header a').setAttribute('data-type', 'track');
+
+	if (!tracks.length) {
+		tracksEl.innerHTML += 'Nothing to display.';
+		return;
+	}
+
 	tracks.forEach(track => {
-		let duration = moment.unix(track.duration).format('m:ss');
-		console.log(duration);
-		ulListEl.innerHTML += `
+		tracksEl.querySelector('ul').innerHTML += `
 			<li class="list-group-item list-group-item-dark">
 				<img src="${track.album.cover}"
 					class="mr-3 cover-img" alt="">
 				<div>
-					<p class="mb-0 font-weight-bold"><a href="#">${track.title}</a> <span class="pl-1 text-muted font-weight-light">${duration}</span></p>
-					<p class="mb-0">by <a href="#">${track.artist.name}</a></p>
-					<p class="text-muted mb-0">${track.album.title}</p>
+					<p class="mb-0">${track.title}</p>
+					<p class="text-muted mb-0">
+						<a href="#" data-artist="${track.artist.id}">${track.artist.name}</a>, 
+						<a href="#" data-album="${track.album.id}">${track.album.title}</a></p>
 				</div>
 			</li>`;
 	});
-
-	searchResultEL.append(ulListEl);
 };
 
-// Fetch data based on search input
-const getSearchResults = async search => {
-	const response = await fetch(
-		`https://deezerdevs-deezer.p.rapidapi.com/search?q=${search}`,
-		{
-			method: 'GET',
-			headers: {
-				'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
-				'x-rapidapi-key':
-					'2a11b8c9e4mshdfa99e565a87ecbp1abf0ejsn25d0e8acdf15'
-			}
-		}
-	);
+// Fetch data based on query-string
+const fetchData = async query => {
+	const response = await fetch(`https://api.deezer.com/${query}`);
 
 	if (!response.ok) {
 		throw new Error('Response was not ok.');
@@ -60,32 +93,34 @@ const getSearchResults = async search => {
 	return await response.json();
 };
 
-const updateUI = (searchResults, search) => {
-	if (searchFilter.includes('track')) {
-		const tracks = filterTracks(searchResults.data, search);
-		renderTracks(tracks, search);
-	}
-	if (searchFilter.includes('album')) {
-		console.log('show albums');
-	}
-	if (searchFilter.includes('artist')) {
-		console.log('show artists');
-	}
+// Get search results based on user input
+const getSearchResults = async search => {
+	const albums = await fetchData(`search/album?q=${search}&limit=5&order=ranking`);
+	const artists = await fetchData(
+		`search/artist?q=${search}&limit=5&order=ranking`
+	);
+	const tracks = await fetchData(`search/track?q=${search}&limit=5&order=ranking`);
+
+	return { artists, albums, tracks };
 };
 
 searchForm.addEventListener('submit', e => {
 	// Prevent default action
 	e.preventDefault();
 
-	// Get search and filter value
+	// Get search value
 	const search = searchForm.search.value.trim().toLowerCase();
-	searchFilter = Array.from(checkBoxes)
-		.filter(checkBox => checkBox.checked)
-		.map(checkBox => checkBox.value);
 
-	// Get search results
+	// Get search result and render to page
 	getSearchResults(search)
-		.then(searchResult => updateUI(searchResult, search))
+		.then(({ artists, albums, tracks }) => {
+			console.log('artists', artists);
+			console.log('albums', albums);
+			console.log('tracks', tracks);
+			renderArtists(artists.data, search);
+			renderAlbums(albums.data, search);
+			renderTracks(tracks.data, search);
+		})
 		.catch(err => {
 			console.log(err);
 		});
